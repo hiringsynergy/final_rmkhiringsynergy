d4bb1b709d24bee1f354d30766826b2fa92a0b08
akashkarthick17 <karthickakash17@gmail.com>
2017-02-23 11:31:29 +0530

    indexx

--- admin_login/index.php
+++ admin_login/index.php
@@ -121,7 +121,7 @@ $tablename=$_GET['tablename'];
   st_jobtype varchar(255),
   st_changemail varchar(255),
   st_changephone varchar(255),
-  st_forgotpassword varchar(255),
+  st_forgotpassword varchar(255)
   
  
 )ENGINE=MyISAM DEFAULT CHARSET=latin1";
