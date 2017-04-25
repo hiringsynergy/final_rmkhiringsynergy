fda020073556748e8e6a255f3e238b2b5d997260
akashkarthick17 <karthickakash17@gmail.com>
2017-04-25 18:31:49 +0530

    dfhdfgj

--- admin_login/search/export_action.php
+++ admin_login/search/export_action.php
@@ -182,7 +182,6 @@ if(! isset($_SESSION['user']) && $_SESSION['user']==null && isset($_SESSION['use
  if(isset($_POST['send_mail'])  && isset($_SESSION['user_role'])=='admin' ){
 
 
-     echo "entered here";
 
     $get_roll= $_POST['check'];
 
