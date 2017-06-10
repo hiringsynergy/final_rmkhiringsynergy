86084a728caa36f843bc20c2eb1ba2fe6995d29c
akashkarthick17 <karthickakash17@gmail.com>
2017-06-10 14:22:35 +0530

    advanced search

--- admin_login/search/advanced_search.php
+++ admin_login/search/advanced_search.php
@@ -676,6 +676,13 @@ function updateSlider(){
                                         <div class="space-32"></div>
                                         <div class="space-32"></div>
 
+                                        <div class="col-xs-12 col-md-4">
+                                            <a href="#" class="btn btn-sm btn-grey">
+                                                <i class="ace-icon "></i>
+                                                View Candidate
+                                            </a>
+                                        </div>
+
 
 
                                         			</div>
