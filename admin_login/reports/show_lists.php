1e7f19759d8e12ca7cc108e5a1fe1495c30c535b
B1ackmamba <kbhitheshsai@gmail.com>
2017-06-15 11:12:29 +0530

    showlist

--- admin_login/reports/show_lists.php
+++ admin_login/reports/show_lists.php
@@ -989,7 +989,7 @@ foreach($checkbox as $list){
 
                                                     $year_of_gradudation = $row_eligible_year['year_of_graduation'];
 
-                                                    $query_count = " SELECT * FROM students_".$year_of_gradudation." WHERE _" .$jid. "='eligible' AND  _" .$jid. "='accepted'";
+                                                    $query_count = " SELECT * FROM students_".$year_of_gradudation." WHERE _" .$jid. "='eligible' OR  _" .$jid. "='accepted' OR _" .$jid. "='placed'";
                                                     $result_count = mysqli_query($connect, $query_count);
                                                     $eligible = mysqli_num_rows($result_count);
 
