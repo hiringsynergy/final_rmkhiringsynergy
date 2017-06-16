e742636676ba44a60cd19fa080007efb5585d055
akashkarthick17 <karthickakash17@gmail.com>
2017-06-16 19:23:55 +0530

    dfghfh

--- admin_login/search/search_action.php
+++ admin_login/search/search_action.php
@@ -2259,8 +2259,8 @@ if(isset($_GET['export'])) {
 
 
 
-//                                                $query_alter="ALTER TABLE students_".$get_year." DROP job_count";
-//                                                $result_alter=mysqli_query($connect,$query_alter);
+                                                $query_alter="ALTER TABLE students_".$get_year." DROP job_count";
+                                                $result_alter=mysqli_query($connect,$query_alter);
 
 
 
