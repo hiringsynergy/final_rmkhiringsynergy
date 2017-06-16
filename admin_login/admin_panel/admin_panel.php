7072f4017d019a9ec0dd04eb1c24064020b8176c
akashkarthick17 <karthickakash17@gmail.com>
2017-06-16 18:37:19 +0530

    xcvbcvb

--- admin_login/admin_panel/admin_panel.php
+++ admin_login/admin_panel/admin_panel.php
@@ -128,16 +128,6 @@ if(! isset($_SESSION['user']) && $_SESSION['user']==null && isset($_SESSION['use
 
 
 
-    <script src="//code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.3.1/js/dataTables.buttons.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="//cdn.rawgit.com/bpampuch/pdfmake/0.1.27/build/pdfmake.min.js"></script>
-    <script src="//cdn.rawgit.com/bpampuch/pdfmake/0.1.27/build/vfs_fonts.js"></script>
-    <script src="//cdn.datatables.net/buttons/1.3.1/js/buttons.html5.min.js">"></script>
-
-
-
 
 
 
