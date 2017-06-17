8cc9dcc19cc377b97cf8e664a94c390e2be61cfe
CHETAN-A <jchetan886@gmail.com>
2017-06-17 14:59:22 +0530

    r

--- admin_login/reports/show_lists.php
+++ admin_login/reports/show_lists.php
@@ -80,7 +80,7 @@ foreach($checkbox as $list) {
     echo $company_new;
     echo "<br>";
 
-    echo $query_unplaced = "UPDATE students_".$year_of_graduation." SET _"."$jid='accepted' st_placementstatus=$company_new WHERE st_roll='$list' ";
+    echo $query_unplaced = "UPDATE students_".$year_of_graduation." SET _"."$jid='accepted' st_placementstatus='$company_new' WHERE st_roll='$list' ";
     $result_unplaced = mysqli_query($connect, $query_unplaced);
 
 //    $query_unplaced1="SELECT * students_".$year_of_graduation." WHERE st_roll='$list' ";
