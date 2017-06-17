214d61e949b073b631e8d073d0d8338491f23140
Vigneshwaran K <vigneshclassic97@gmail.com>
2017-06-17 11:59:32 +0530

    Profile

--- admin_login/profile/profile.php
+++ admin_login/profile/profile.php
@@ -1756,8 +1756,10 @@ if(isset($_GET['roll'])&& isset($_SESSION['user_role'])=='admin')
                                                             </div>
                                                         </div>
                                                     </div>
+
                                                     <?php if((strcasecmp($row['st_currentlypursuing'],"ug"))){
                                                         ?>
+
                                                     <div class="space-4"></div>
                                                     <h3  style="color: black; font-weight: bold; text-align:inherit ; padding-left: 12px ;font-size: 18px;" >PG</h3>
 
@@ -2466,7 +2468,7 @@ if(isset($_GET['roll'])&& isset($_SESSION['user_role'])=='admin')
                                                                 </div>
                                                             </div>
 
-                                                            <?php if((strcasecmp($row['st_currentlypursuing'],"pg"))){
+                                                            <?php if((strcasecmp($row['st_currentlypursuing'],"ug"))){
                                                             ?>
 
                                                             <div class="form-group">
@@ -2594,7 +2596,7 @@ if(isset($_GET['roll'])&& isset($_SESSION['user_role'])=='admin')
                                                                 </div>
                                                             </div>
 
-                                                            <?php }?>
+                                                            <?php } ?>
 
                                                             <div class="space-4"></div>
 
