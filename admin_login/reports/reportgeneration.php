e4b189ad6b6d81dec97f0c5b8fd074465b8ef3bb
akashkarthick17 <karthickakash17@gmail.com>
2017-06-17 10:24:57 +0530

    dtnfgh

--- admin_login/reports/reportgeneration.php
+++ admin_login/reports/reportgeneration.php
@@ -829,7 +829,7 @@ if(! isset($_SESSION['user']) && $_SESSION['user']==null && isset($_SESSION['use
                             <div class="space-16"></div>
                         </div>
 
-                        <div class="row">
+                        <div class="row col-xs-12">
                             <div class="col-xs-6 col-xs-push-2">
 
 
