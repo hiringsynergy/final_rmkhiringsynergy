685d872733159ad161bd731474325cca43ec8157
akashkarthick17 <karthickakash17@gmail.com>
2017-06-17 10:40:31 +0530

    fdgh

--- admin_login/reports/reportgeneration.php
+++ admin_login/reports/reportgeneration.php
@@ -835,14 +835,15 @@ if(! isset($_SESSION['user']) && $_SESSION['user']==null && isset($_SESSION['use
                             <div class="space-16"></div>
                         </div>
 
-                        <div class="bigger-170 blue">
-                            Reports
 
-
-
-                        </div>
 
                         <div class="row">
+                            <div class="bigger-170 blue">
+                                Reports
+
+
+
+                            </div>
                             <div class="col-xs-12">
                                 <div class="col-xs-6 col-xs-push-2">
 
