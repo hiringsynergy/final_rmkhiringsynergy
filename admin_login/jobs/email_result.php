4fcbb2e92dad799394d524eb356dae1f9702b619
akashkarthick17 <karthickakash17@gmail.com>
2017-06-20 12:23:06 +0530

    sdfgdfgf

--- admin_login/jobs/email_result.php
+++ admin_login/jobs/email_result.php
@@ -581,7 +581,7 @@ if (!isset($_SESSION['user']) && $_SESSION['user'] == null && $_SESSION['user_ro
 
                     $mail=new PHPMailer();
 
-                    $mail->isSMTP();
+                    $mail->isMail();
                     $mail->Host = 'mx1.hostinger.com';  // Specify main and backup SMTP servers
                     $mail->SMTPAuth = true;    // Enable SMTP authentication
 
@@ -590,27 +590,6 @@ if (!isset($_SESSION['user']) && $_SESSION['user'] == null && $_SESSION['user_ro
 
 
 
-                    require_once 'swiftmailer-master/lib/swift_required.php';
-
-// Create the Transport
-                    $transport = (new Swift_SmtpTransport('mx1.hostinger.com', 587));
-                        $transport->setUsername('rmdplacements@rmkhiringsynergy.xyz');
-                        $transport->setPassword('rmkhiringsynergy');
-
-
-// Create the Mailer using your created Transport
-                    $mailer = new Swift_Mailer($transport);
-
-// Create a message
-                    $message = new Swift_Message('Wonderful Subject');
-                         $message->setFrom(['rmdplacements@rmkhiringsynergy.xyz' => 'John Doe']);
-
-                        $message->setBody('Here is the message itself');
-
-
-
-// Send the message
-                        //$result = $mailer->send($message);
 
 
 
@@ -779,8 +758,8 @@ if (!isset($_SESSION['user']) && $_SESSION['user'] == null && $_SESSION['user_ro
 
                         $to = $row_mail['st_clgemail'];
 
-                        //$mail->addAddress($to);     // Add a recipient
-                        $message->setTo([$to, $to => 'A name']);
+                        $mail->addAddress($to);     // Add a recipient
+
 
 
                         if (isset($_FILES['attachment']) && $file_ext != '' && isset($_SESSION['user_role']) == 'admin') {
@@ -796,18 +775,18 @@ if (!isset($_SESSION['user']) && $_SESSION['user'] == null && $_SESSION['user_ro
                         }
 
 
-                        if ($mailer->send($message)) {
+                        if ($mail->send()) {
 
 
                             $counter = $counter + 1;
 
-                            echo "message sent";
+                            //echo "message sent";
 
                         }
                         else{
 
 
-                           echo  "not sent";//$mail->ErrorInfo;
+                           //echo  "not sent";//$mail->ErrorInfo;
                         }
 
 
