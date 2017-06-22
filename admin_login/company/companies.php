89a925226169e524cefe47ba68c7e59a5bf19d27
Vigneshwaran K <vigneshclassic97@gmail.com>
2017-06-22 22:41:51 +0530

    Changes

--- admin_login/company/companies.php
+++ admin_login/company/companies.php
@@ -61,27 +61,7 @@ if(! isset($_SESSION['user']) && $_SESSION['user']==null && isset($_SESSION['use
             location.href = "../settings";
 
         }
-        function showUser(str) {
-            if (str == "") {
-                document.getElementById("modal-form").innerHTML = "";
-                return;
-            } else {
-                if (window.XMLHttpRequest) {
-                    // code for IE7+, Firefox, Chrome, Opera, Safari
-                    xmlhttp = new XMLHttpRequest();
-                } else {
-                    // code for IE6, IE5
-                    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-                }
-                xmlhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        document.getElementById("modal-form").innerHTML = this.responseText;
-                    }
-                };
-                xmlhttp.open("GET","../company/getuser?id="+str,true);
-                xmlhttp.send();
-            }
-        }
+        
 
 
 
