b552df294dc16b3e95af88df8b4b6711d560527d
Vigneshwaran K <vigneshclassic97@gmail.com>
2017-06-23 00:03:44 +0530

    link change

--- admin_login/index.php
+++ admin_login/index.php
@@ -775,7 +775,7 @@ if(isset($_GET['delete']) && isset($_SESSION['user_role'])=='admin' )
 						<ul class="breadcrumb">
 							<li>
 								<i class="ace-icon fa fa-home home-icon"></i>
-								<a href="../index">Home</a>
+								<a href="index">Home</a>
 							</li>
 							<li class="active">Dashboard</li>
 						</ul><!-- /.breadcrumb -->
