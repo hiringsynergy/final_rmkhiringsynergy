8732fbbcbdc9b285ed527bc955adcf968f8d7f72
B1ackmamba <kbhitheshsai@gmail.com>
2017-06-29 14:57:55 +0530

    kjhkjh

--- admin_login/admin_panel/admin_panel.php
+++ admin_login/admin_panel/admin_panel.php
@@ -864,11 +864,11 @@ if(isset($_POST['update_submit']) && isset($_SESSION['user_role'])=='admin' ) {
                                         Number of students :
                                         <?php
 
-                                        $query="select * from students_".$table." ";
-                                        $result=mysqli_query($connect,$query);
-                                        $c = count($result);
+                                        $query1="select * from students_".$table." ";
+                                        $result1=mysqli_query($connect,$query1);
+                                        $c = count($result1);
 
-                                        echo $c;
+                                        echo $table;
 
                                         ?></div>
                                 <div class="table-header">
