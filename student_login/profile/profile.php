e30009c71a62fad5f9f7d66dc8d1a3ae8cc4c392
yuva <yuvaammu3@gmail.com>
2017-07-04 10:45:14 +0530

    profile for all three

--- student_login/profile/profile.php
+++ student_login/profile/profile.php
@@ -2498,14 +2498,29 @@ if (isset($_POST['skill']) && isset($_SESSION['user_role'])=='student') {
                                                             <div class="profile-info-name align-left test " style="background:#F1A9A0; color:#D91E18" > <b>Placed in companies</b></div>
 
                                                             <div class="profile-info-value testred">
-                                                                <span class="editable" id="ur"><?php  echo substr($row['st_placementstatus'],1);  ?></span>
+                                                                <span class="editable" id="ur"><?php  echo$row['st_placementstatus'];  ?></span>
                                                             </div>
                                                         </div>
 
 
 
                                                     </div>
+
                                                     <div class="space-10"></div>
+
+                                                    <div class="profile-user-info profile-user-info-striped bigger-110 bolder">
+
+                                                        <div class="profile-info-row  ">
+                                                            <div class="profile-info-name align-left test " style="background:#F1A9A0; color:#D91E18" > <b>Opted Company</b></div>
+
+                                                            <div class="profile-info-value testred">
+                                                                <span class="editable" id="ur"><?php  echo$row['st_opted'];  ?></span>
+                                                            </div>
+                                                        </div>
+
+
+
+                                                    </div>
 
                                                 </div>
 
