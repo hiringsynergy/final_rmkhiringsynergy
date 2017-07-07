d5c0ff734ab90e1f33f67becd85f82b29635a9e7
akashkarthick17 <karthickakash17@gmail.com>
2017-07-07 22:41:01 +0530

    dgsdfg

---
+++ verify.php
@@ -0,0 +1,15 @@
+<?php
+ob_start();
+session_start();
+
+
+/**
+ * Created by PhpStorm.
+ * User: Akash
+ * Date: 07-07-2017
+ * Time: 22:39
+ */
+
+
+header("Location :https://www.rmkcampulse.com");
+
