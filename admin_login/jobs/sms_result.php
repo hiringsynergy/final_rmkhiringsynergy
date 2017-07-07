544d0572d96b218d9e86f4d4c49ceb6c9bcd428b
akashkarthick17 <karthickakash17@gmail.com>
2017-07-07 21:05:12 +0530

    fvb

--- admin_login/jobs/sms_result.php
+++ admin_login/jobs/sms_result.php
@@ -574,9 +574,39 @@ if (!isset($_SESSION['user']) && $_SESSION['user'] == null && $_SESSION['user_ro
 
 
 
+// Replace with your username
+                    $user = "Rmkcampulsesms";
+
+// Replace with your API KEY (We have sent API KEY on activation email, also available on panel)
+                    $apikey = "t2V9czTh0bi39lGGUYLN";
+
+// Replace if you have your own Sender ID, else donot change
+                    $senderid  =  "MYTEXT";
+
+// Replace with the destination mobile Number to which you want to send sms
+                    $mobile  =  "9566007442";
+
+// Replace with your Message content
+                    $message   =  "Testing SMS API";
+                    $message = urlencode($message);
+
+// For Plain Text, use "txt" ; for Unicode symbols or regional Languages like hindi/tamil/kannada use "uni"
+                    $type   =  "txt";
+
+                    $ch = curl_init("http://smshorizon.co.in/api/sendsms.php?user=".$user."&apikey=".$apikey."&mobile=".$mobile."&senderid=".$senderid."&message=".$message."&type=".$type."");
+                    curl_setopt($ch, CURLOPT_HEADER, 0);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    $output = curl_exec($ch);
+                    curl_close($ch);
+
+// Display MSGID of the successful sms push
+                    echo $output;
 
 
-                    header("Location: post_jobs");
+
+
+
+                   // header("Location: post_jobs");
 
                 }
 
