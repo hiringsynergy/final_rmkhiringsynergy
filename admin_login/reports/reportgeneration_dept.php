31eb5cb319447e2d545c9526be2ab32502eec40a
CHETAN <jchetan886@gmail.com>
2017-08-18 14:56:25 +0530

    sdfg

--- admin_login/reports/reportgeneration_dept.php
+++ admin_login/reports/reportgeneration_dept.php
@@ -719,14 +719,7 @@ if(! isset($_SESSION['user']) && $_SESSION['user']==null && isset($_SESSION['use
                                              <td>CSE</td>
                                              <td>
                                                  <?php
-                                                 
-
-                                                 
-
-
-                                                 
-                                                 
-                                                 
+                                   
 
                                                     $query_students_cse = "SELECT * FROM students_".$table."  WHERE st_ugspecialization='cse'";
 
@@ -763,7 +756,7 @@ if(! isset($_SESSION['user']) && $_SESSION['user']==null && isset($_SESSION['use
                                              echo $table;
 
                                              $query_single_cse = " SELECT * FROM students_".$table." WHERE st_jobcounts = 1 AND st_ugspecialization = 'cse' ";
-                                             $result_single_cse = myssqli_query($connect,$query_single_cse);
+                                             $result_single_cse = mysqli_query($connect,$query_single_cse);
 
                                               if($result_single_cse==''){
 
