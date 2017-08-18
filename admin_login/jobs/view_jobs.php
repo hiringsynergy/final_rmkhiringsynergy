77aecbf6fa97e8f8c287f29677e69556650759ec
akashkarthick17 <karthickakash17@gmail.com>
2017-08-18 12:06:06 +0530

    jhghjk

--- admin_login/jobs/view_jobs.php
+++ admin_login/jobs/view_jobs.php
@@ -874,7 +874,7 @@ if(isset($_POST['update']) && isset($_SESSION['user_role'])=='admin'  )
                         if(isset($_GET['year']) && isset($_SESSION['user_role'])=='admin' )
                         {
                             echo $year=$_GET['year'];
-                            $query="SELECT * FROM jobs WHERE year_of_graduation = '$year' ORDER BY sort DESC ";
+                            $query="SELECT * FROM jobs WHERE year_of_graduation = '$year' ";
                         }
                         else
                         {
