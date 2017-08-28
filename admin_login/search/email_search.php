9c4ebbd17dae96047a1b759d744ddcaa4310bb12
akashkarthick17 <karthickakash17@gmail.com>
2017-08-28 18:17:41 +0530

    dfgdfg

--- admin_login/search/email_search.php
+++ admin_login/search/email_search.php
@@ -844,51 +844,53 @@ if(! isset($_SESSION['user']) && $_SESSION['user']==null && isset($_SESSION['use
                     $query_send_mail= "SELECT * FROM mail_forwarder";
                     $result_send_mail=mysqli_query($connect,$query_send_mail);
 
-//                    while($row_send_mail=mysqli_fetch_assoc($result_send_mail)){
-//
-//                        $to = $row_send_mail['tnp'];
-//
-//
-//                        $mail->addAddress($to,$to);     // Add a recipient
-//
-//                        echo $subject."<br>";
-//                        echo $message."<br>";
-//                        echo $collegename."<br>";
-//
-//
-//                        if (isset($_FILES['attachment']) && $file_ext != '' && isset($_SESSION['user_role']) == 'admin') {
-//
-//                            foreach ($send_file as $file_to_send) {
-//
-//
-//                                $mail->addAttachment('../files/' . $file_to_send, $file_to_send);
-//
-//                            }
-//
-//
-//                        }
-//
-//
-//                        if ($mail->send()) {
-//
-//
-//                            echo $to."<br>"."hurray"."<br>";
-//
-//
-//                        }
-//                        else{
-//
-//                            echo  $mail->ErrorInfo." ".$to;
-//                        }
-//
-//
-//                        // Clear all addresses and attachments for next loop
-//                        $mail->clearAddresses();
-//                        $mail->clearAttachments();
-//
-//
-//
-//                    }
+                    while($row_send_mail=mysqli_fetch_assoc($result_send_mail)){
+
+                        $to = $row_send_mail['akash'];
+
+                        if($to!='') {
+
+                            $mail->addAddress($to, $to);     // Add a recipient
+
+
+                            echo $subject . "<br>";
+                            echo $message . "<br>";
+                            echo $collegename . "<br>";
+
+
+                            if (isset($_FILES['attachment']) && $file_ext != '' && isset($_SESSION['user_role']) == 'admin') {
+
+                                foreach ($send_file as $file_to_send) {
+
+
+                                    $mail->addAttachment('../files/' . $file_to_send, $file_to_send);
+
+                                }
+
+
+                            }
+
+
+                            if ($mail->send()) {
+
+
+                                echo $to . "<br>" . "hurray" . "<br>";
+
+
+                            } else {
+
+                                echo $mail->ErrorInfo . " " . $to;
+                            }
+
+
+                            // Clear all addresses and attachments for next loop
+                            $mail->clearAddresses();
+                            $mail->clearAttachments();
+
+
+                        }
+
+                    }
 
 
 
